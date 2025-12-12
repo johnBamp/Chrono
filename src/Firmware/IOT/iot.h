a73d73d9c978ce3a204cbd01c2f4fcaa37490794
John Bampfield <johnreidbampfield@example.com>
2025-12-12 10:12:02 -0500

    Add initial firmware files for Clock, IMU, IOT, Microphone, Screen, and Touchscreen components

---
+++ src/Firmware/IOT/iot.h
@@ -0,0 +1,6 @@
+#include "Hardware/hardware.h"
+
+//Button #1 pin
+//Button #2 pin
+//Battery moniter pin
+//Haptic motor pin
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include "Hardware/hardware.h"
 
+
+//No software or app should be able to acess the screen directly, only through the canvas object and telling it to draw to the screen.
+//Same goes for all other sensors.
+
 void setup() {
 
 }
